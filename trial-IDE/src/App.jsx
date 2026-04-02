6b2a06e1821e4b8dafaba1512d03d88abc3dc995
Akshat Mahajan <akshatcsfb2296@svvv.edu.in>
2026-04-02 04:08:51 +0000

    logs page UI updated

--- trial-IDE/src/App.jsx
+++ trial-IDE/src/App.jsx
@@ -1,7 +1,11 @@
 import Editor from './pages/Editor.jsx'
+import Logs from './pages/Logs.jsx'
+import LogsTwo from './pages/LogsTwo.jsx'
 
 function App() {
-  return <Editor />
+  // return <Editor />
+  // return <Logs />
+  return <LogsTwo />
 }
 
 export default App

ed775a8b08c4997232ce67b4262ae638cdce511e
Akshat Mahajan <akshatcsfb2296@svvv.edu.in>
2026-04-03 10:42:24 +0000

    Layout addons

--- trial-IDE/src/App.jsx
+++ trial-IDE/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/Layout.jsx'
 import Editor from './pages/Editor.jsx'
 import LogsTwo from './pages/LogsTwo.jsx'
 import Profile from './pages/Profile.jsx'
@@ -7,13 +8,15 @@ import Home from './pages/Home.jsx'
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/editor" element={<Editor />} />
-        <Route path="/logs2" element={<LogsTwo />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/editor" element={<Editor />} />
+          <Route path="/logs2" element={<LogsTwo />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
